c749ffe1ac209ac9d23a135e1a1c84e46429bdaf
Trung Nguyen <ndt8895@gmail.com>
2017-10-03 13:21:43 +0700

    Model added

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,4 +1,5 @@
 class Order < ApplicationRecord
+  belongs_to :order_status
   has_many :ordered_items
   has_many :items, though: :ordered_items
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170927180026) do
+ActiveRecord::Schema.define(version: 20171003061929) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -54,11 +54,18 @@ ActiveRecord::Schema.define(version: 20170927180026) do
     t.index ["category_id"], name: "index_items_on_category_id"
   end
 
+  create_table "order_statuses", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "ordered_items", force: :cascade do |t|
     t.bigint "order_id"
     t.bigint "item_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "quantity"
     t.index ["item_id"], name: "index_ordered_items_on_item_id"
     t.index ["order_id"], name: "index_ordered_items_on_order_id"
   end
