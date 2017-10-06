eeb80850e82a7f5646d30053adad8c3b20ca3e0e
Trung Nguyen <ndt8895@gmail.com>
2017-10-06 10:08:35 +0700

    Set up js assets for admin page

---
+++ app/assets/javascripts/application-admin.js
@@ -0,0 +1,11 @@
+//= require rails-ujs
+//= require turbolinks
+//= require prototype
+//= require jquery-1.10.2.min
+//= require toastr
+//= require adminlte.min
+//= require demo
+//= require fastclick
+//= require jquery.slimscroll.min
+
+
