82d924c4d62a32daa185b28e814d007d5088b25b
Trung Nguyen <ndt8895@gmail.com>
2017-10-27 07:05:31 +0700

    shopping page

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,7 +1,11 @@
 class CategoriesController < ApplicationController
   before_action :load_categories
+  before_action :load_cat
   def show
-
   end
 
+  private
+  def load_cat
+    @category = Category.find_by_id params[:id]
+  end
 end
---
+++ app/helpers/bread_crumbs_helper.rb
@@ -0,0 +1,21 @@
+module BreadCrumbsHelper
+  def crumb crum =[], category
+    curmb = ''
+    curmb << "<li class='category7'>"
+    if crum.size > 0
+      curmb << "<a href='#'>"
+    end
+    curmb << "<strong>#{category.name}</strong>"
+    if crum.size > 0
+      curmb << '</a>'
+    end
+    if crum.size > 0
+      curmb << "<span class='fa fa-angle-right'></span>"
+    end
+    curmb << "</li>"
+    crum.unshift(curmb)
+    unless category.parent.blank?
+      crumb(crum, category.parent)
+    end
+  end
+end
