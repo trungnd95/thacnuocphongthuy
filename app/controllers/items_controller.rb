71ccafd81360f9d3655507bedc293858ac1c79b3
Trung Nguyen <ndt8895@gmail.com>
2017-11-07 08:24:17 +0700

    Image zoom in detail page done

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,11 +2,12 @@ class ItemsController < ApplicationController
   before_action :load_categories
   before_action :load_item, only: :show
   before_action :load_sys_info
+
   def show
   end
 
   private
   def load_item
-    @item = Item.find_by_id(params[:id])
+    @item = Item.includes(:images).find_by_id(params[:id])
   end
 end
